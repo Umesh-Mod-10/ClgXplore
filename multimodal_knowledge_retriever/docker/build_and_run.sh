47d68123785fdd5de224e214b5856322ad29a569
Umesh <umesh.ramanathan2004@gmail.com>
2024-09-14 17:04:13 +0530

    first commit

--- multimodal_knowledge_retriever/docker/build_and_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build Docker image
-docker build -t multimodal_knowledge_retriever -f docker/Dockerfile ..
-
-# Run Docker container
-docker run -p 8501:8501 -v $(pwd)/data:/app/multimodal_knowledge_retriever/data multimodal_knowledge_retriever
